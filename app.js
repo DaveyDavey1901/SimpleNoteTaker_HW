e363cae64ee9a5fbd242bdea37f339f952b4dfde
David Davies <David_Davies@mail.com>
2022-01-24 00:19:11 +0000

    started adding some routes added installed uuid for possible use later

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const path = require("path");
 const nodemon = require('nodemon');
-const PORT = process.env.PORT || 5001;
 
+const PORT = process.env.PORT || 5001;
 const app = express();
 
 // Body Parser Middleware
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+// routes 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
+//Listening on the port for activity alerts when started
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+
+
+
