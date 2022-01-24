fc7e196521384f9ef9a09919122e42c665791695
David Davies <David_Davies@mail.com>
2022-01-24 00:35:05 +0000

    update some functions need to finish the post and delete for the notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,12 @@
-
 var db = require("../db/db.json"); //users notes will save to here
 var fs = require("fs");
 
-
+module.exports = (app) => {
+  app.get("/api/notes",  (req, res) => {
+    res.json(db);
+  });
+//to do
+  app.post("/api/notes", (req, res) => {});
+//to do
+  app.delete("/api/notes/:id",  (req, res) =>{});
+};
