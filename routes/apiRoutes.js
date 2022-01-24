09bb6cc06d42fa6df63e76b1fc03a287c39dfaf9
David Davies <David_Davies@mail.com>
2022-01-24 14:30:58 +0000

    added the post request to add a new note to the json file with an id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,34 @@
 var db = require("../db/db.json"); //users notes will save to here
 var fs = require("fs");
+const uuid = require("uuid");
 
 module.exports = (app) => {
   app.get("/api/notes",  (req, res) => {
     res.json(db);
   });
+
+  app.post("/api/notes", (req, res) => {
+    // Add an id to each new note
+    const note = {
+      id: db.length+1,
+      title: req.body.title,
+      text: req.body.text,
+    };
+    // Send 404 error (meaning the object doesn't exist and cannot be found)
+       if (!note.title || !note.text) {
+      return res.status(404).json({ msg: "Please add a title as well as the note" });
+    }
+    //Attach new note to the notes array.
+    db.push(note);
+    
+    //write new notes to db.json file
+    fs.writeFile("./db/db.json", JSON.stringify(db), (err) => {
+      if (err) throw err;
+      console.log("Writing to json file");
+    });
+    res.send(note);
+  });
+
 //to do
-  app.post("/api/notes", (req, res) => {});
-//to do
-  app.delete("/api/notes/:id",  (req, res) =>{});
+ // app.delete("/api/notes/:id",  (req, res) =>{});
 };
